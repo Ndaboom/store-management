9d6e22221661a878c92c50e902c312ab6cf137db
ndaboom <ndabosam084@gmail.com>
2022-11-28 01:05:38 +0100

    Login UI implemented

---
+++ src/auth/loginUI.java
@@ -0,0 +1,109 @@
+package auth;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+public class loginUI {
+
+	private JFrame frmAuthentification;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		// Look and feel 
+		try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(loginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(loginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(loginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(loginUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+		// Look and feel
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					loginUI window = new loginUI();
+					window.frmAuthentification.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public loginUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmAuthentification = new JFrame();
+		frmAuthentification.setBackground(Color.WHITE);
+		frmAuthentification.setTitle("Authentification");
+		frmAuthentification.setBounds(100, 100, 437, 314);
+		frmAuthentification.setResizable(false);
+		frmAuthentification.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAuthentification.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setToolTipText("Entrez votre nom d'utilisateur");
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(0, 0, 428, 275);
+		frmAuthentification.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Authentification");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 18));
+		lblNewLabel.setBounds(140, 21, 167, 42);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(59, 99, 297, 30);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nom d'utilisateur");
+		lblNewLabel_1.setBounds(59, 74, 295, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Mot de passe");
+		lblNewLabel_2.setBounds(59, 145, 295, 14);
+		panel.add(lblNewLabel_2);
+		
+		passwordField = new JPasswordField();
+		passwordField.setToolTipText("Entrez votre mot de passe");
+		passwordField.setBounds(59, 169, 295, 30);
+		panel.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Commencer");
+		btnNewButton.setBounds(162, 210, 112, 40);
+		btnNewButton.setForeground(new java.awt.Color(0, 51, 255));
+		panel.add(btnNewButton);
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,5 @@
  */
 module StoreManagement {
 	requires java.desktop;
+	requires java.logging;
 }
