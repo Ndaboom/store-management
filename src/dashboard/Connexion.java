5a9f5634b2f9e043d0c415419edbf923f3d16d02
Sammy Ndabo <ndabosam084@gmail.com>
2023-11-01 11:02:41 +0200

    Gain & tithes calculation

--- src/dashboard/Connexion.java
+++ src/dashboard/Connexion.java
@@ -2,26 +2,26 @@ package dashboard;
 import java.sql.*;
 
 public class Connexion {
-    String urlPilote="com.mysql.jdbc.Driver";
-    String urlBaseDonnees="jdbc:Mysql://localhost:3306/store-management";//Chemin de connexion a la base des donnees
+	String urlPilote = "com.mysql.cj.jdbc.Driver";
+    String urlBaseDonnees="jdbc:mysql://localhost:8889/store-management";//Chemin de connexion a la base des donnees
     Connection con;
-        public Connexion(){
+    public Connexion(){
     try{
     Class.forName(urlPilote);
     System.out.println("Chargement du pilote de réussi");
     
-}catch(ClassNotFoundException ex){
+    }catch(ClassNotFoundException ex){
     System.out.println(ex);
-}
-    try{
-        con=DriverManager.getConnection(urlBaseDonnees,"root","");
-        System.out.println("Connexion à la base de données réussi");
+    }
     
-}catch(SQLException ex){
-    System.out.println(ex);
-}
-}
-        public Connection ObtenirConnexion(){
+    try{
+        con=DriverManager.getConnection(urlBaseDonnees,"root","root");
+        System.out.println("Connexion à la base de données réussi");
+    }catch(SQLException ex){
+    	System.out.println(ex);
+    }
+    }
+    public Connection ObtenirConnexion(){
         return con;
     }
 
